b77790f09050388539274a1548f27822f45491ed
Kayla Lawson <kaylarae7128@gmail.com>
2019-09-14 16:55:48 -0600

    Finalize fetch calls

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import $ from 'jquery';
 import './css/base.scss';
 import domUpdates from './domUpdates.js';
 import Hotel from './Hotel';
+import { getGuests, getRooms, getBookings, getServices } from './Fetch.js';
+// import {getRooms} from './Fetch';
+// import {getBookings} from './Fetch.js';
+// import {getServices} from './Fetch.js';
 
 
 $( document ).ready(async function () {
@@ -9,45 +13,45 @@ $( document ).ready(async function () {
   hotel.startHotel(await getGuests(), await getRooms(), await getBookings(), await getServices())
   console.log(hotel)
 
-async function getGuests() {
-  try {
-   let response = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
-   const data = await response.json()
-    return data.users
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function getGuests() {
+//   try {
+//    let response = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+//    const data = await response.json()
+//     return data.users
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-async function getRooms() {
-  try {
-  let response = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
-    const data = await response.json()
-    return data.rooms
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function getRooms() {
+//   try {
+//   let response = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+//     const data = await response.json()
+//     return data.rooms
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-async function getBookings() {
-  try {
-  let response = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings');
-      const data = await response.json()
-       return data.bookings
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function getBookings() {
+//   try {
+//   let response = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings');
+//       const data = await response.json()
+//        return data.bookings
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
 
-async function getServices() {
-  try {
-    let response = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices')
-      const data = await response.json() 
-      return data.roomServices
-  } catch (error) {
-    console.log(error)
-  }
-}
+// async function getServices() {
+//   try {
+//     let response = await fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices')
+//       const data = await response.json() 
+//       return data.roomServices
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
   hotel.findDate();
   domUpdates.displayTodaysDate(hotel.displayDate); 
